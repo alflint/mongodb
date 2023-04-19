cba9c4ffb22577966d6a9780bf7ff0e4d00679d5
alfint <alexandreaflint1111@gmail.com>
2023-04-18 18:58:52 -0600

    added morgan for logging

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const morgan = require('morgan');
 
 app.use(bodyParser.json());
+app.use(morgan(':method :url'));
 
 // Import routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/thoughts', require('./routes/thoughts'));
-
 
 function start_express() {
   app.listen(3000, () => {
